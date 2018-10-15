6f97e7f9bf4680e49e606aec42ee3bb6b2c07599
= <=>
2018-10-14 19:06:32 -0700

    updated to work on uas-slack

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 
 
 var bot = new SlackBot({
-    token: 'xoxb-451879041059-452016292354-nBs4wDMAlzFiu84vb3oD5TMU', //change token based on workspace 
+    token: 'xoxb-85202238740-456015486690-dP7EbMbojSXQD04IBsxpBk83', //change token based on workspace 
     name: 'memebot' //name chosen at bot setup in workspace
 
 });
 
 
 bot.on('start', () => {
-    bot.postMessageToChannel('1_memes', 'memebot online');    
+    //bot.postMessageToChannel('bots_setup', 'memebot online');    
 });
 
 bot.on('message', data => {
@@ -19,7 +19,7 @@ bot.on('message', data => {
     if (data.type == 'message')
     {
         console.log(data.text);
-        if (data.text.includes("<@UDA0G8LAE")) //if the bots tag is mentioned in the chat (id: may change based on workspace)
+        if (data.text.includes("<@UDE0FEALA")) //if the bots tag is mentioned in the chat (id: may change based on workspace)
         {
             getMeme();
         }
@@ -34,14 +34,14 @@ function getMeme()
         
         var post_index = Math.floor(Math.random()*(10-0+1)+0); //get a random post index from the top 10
         var title = res.data.data.children[post_index].data.title;
-        bot.postMessageToChannel('1_memes', title);
+        bot.postMessageToChannel('bots_setup', title);
         
         var meme = res.data.data.children[post_index].data.url;  
         if (meme.includes("imgur"))     
         {
             meme += ".jpg"  //imgur links dont go directly to a file so we can try to append .jpg
         }
-        bot.postMessageToChannel('1_memes', meme);
+        bot.postMessageToChannel('bots_setup', meme);
 
     });
 
